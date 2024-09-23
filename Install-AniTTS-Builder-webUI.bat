d233b4bc6299570a6bc21c4dbbd31e5d8c083aa5
N01N9 <060311sdh@gmail.com>
2024-09-23 19:18:53 +0900

    Update Install-AniTTS-Builder-webUI.bat

--- Install-AniTTS-Builder-webUI.bat
+++ Install-AniTTS-Builder-webUI.bat
@@ -1,28 +1,28 @@
 @echo off
 chcp 65001 > NUL
 
-REM 에러코드를 위한 설정
+REM Enable delayed variable expansion for error handling
 setlocal enabledelayedexpansion
 
-REM PowerShell 및 curl 명령어 설정
+REM PowerShell and curl command configurations
 set PS_CMD=PowerShell -Version 5.1 -ExecutionPolicy Bypass
 set CURL_CMD=C:\Windows\System32\curl.exe
 
-REM PortableGit URL 및 저장 경로 설정
+REM PortableGit download URL and destination path
 set GIT_DL_URL=https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/PortableGit-2.44.0-64-bit.7z.exe
 set GIT_DL_DST=%~dp0lib\PortableGit-2.44.0-64-bit.7z.exe
 
-REM Conda 다운로드 URL 및 저장 경로 설정
+REM Miniconda download URL and destination path
 set CONDA_DL_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
 set CONDA_DL_DST=%~dp0lib\Miniconda3-latest-Windows-x86_64.exe
 
-REM 리포지토리 URL 설정
+REM Repository URL to clone
 set REPO_URL=https://github.com/N01N9/AniTTS-Builder-webUI.git
 
-REM 현재 경로를 bat 파일 위치로 이동
+REM Navigate to the batch file's directory
 pushd %~dp0
 
-REM lib 폴더가 없다면 생성
+REM Create the lib folder if it doesn't exist
 if not exist lib\ ( mkdir lib )
 
 echo --------------------------------------------------
@@ -86,17 +86,17 @@ echo --------------------------------------------------
 git clone %REPO_URL%
 if !errorlevel! neq 0 ( echo Failed to clone repository. Exiting... & pause & exit /b 1 )
 
-REM 클론한 리포지토리 내의 yaml 파일로 conda 환경 생성
+REM Create Conda environment from the YAML file in the cloned repository
 set REPO_DIR=AniTTS-Builder-webUI
-if exist "%REPO_DIR%\environment.yaml" (
+if exist "%REPO_DIR%\AniTTS-Builder-env.yaml" (
     echo --------------------------------------------------
-    echo Found environment.yaml. Creating Conda environment...
+    echo Found AniTTS-Builder-env.yaml. Creating Conda environment...
     echo --------------------------------------------------
-    conda env create -f "%REPO_DIR%\environment.yaml"
+    conda env create -f "%REPO_DIR%\AniTTS-Builder-env.yaml"
     if !errorlevel! neq 0 ( echo Failed to create Conda environment. Exiting... & pause & exit /b 1 )
 ) else (
     echo --------------------------------------------------
-    echo environment.yaml not found. Skipping Conda environment creation...
+    echo AniTTS-Builder-env.yaml not found. Skipping Conda environment creation...
     echo --------------------------------------------------
 )
 
